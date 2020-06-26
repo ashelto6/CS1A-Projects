48cc29d5fd663e7c9575253bd711e55a03ee3218
Antonio Shelton-McGaha <60279886+ashelto6@users.noreply.github.com>
2020-06-26 19:09:00 -0400

    Add files via upload

---
+++ Lab1_FirstProgram/Lab1_FirstProgram/firstProgram.cpp
@@ -0,0 +1,11 @@
+// Greetings from an ELITE Programmer (to be)
+// Antonio Shelton-McGaha
+// 1/17/2020
+
+#include <iostream>
+
+using std::cout; using std::endl;
+
+int main() {
+	cout << "Hello Antonio, How are you?" << endl;
+}
